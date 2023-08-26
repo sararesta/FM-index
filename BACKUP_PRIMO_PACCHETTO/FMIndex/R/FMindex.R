b79735f5b55fbbf3e879afc870094e44ae2fd9ff
Sara Resta <sara.resta00@gmail.com>
2023-08-26 15:28:54 +0200

    Update FMindex.R

--- BACKUP_PRIMO_PACCHETTO/FMIndex/R/FMindex.R
+++ BACKUP_PRIMO_PACCHETTO/FMIndex/R/FMindex.R
@@ -10,9 +10,7 @@
 #' it must end with '/' (you can leave it empty if you want to save the files 
 #' in your working directory).
 #' @param tallywidth How you want to compress the tally table. Default value=1
-#' @param includeTerminationCharacter default value: true
-#' @param includeSA set it to TRUE if you want to print also the
-#' suffix array.
+#' @param includeEndChar default value: true
 #' @return A list containint all the data structures necessary for the FM index
 #' @examples 
 #' FMindex(system.file("extdata/examples/seq5.fasta",package="FMIndex"),"")
@@ -21,32 +19,30 @@
 #' @export
 #' @importFrom Biostrings readDNAStringSet
 #' @importFrom utils write.table
-FMindex <- function(fastafile,usrpath,tallywidth=1,
-                    includeTerminationCharacter=TRUE,
-                    includeSA=FALSE){
-    seqset <- readDNAStringSet(fastafile,use.names=FALSE)
+FMindex <- function(fastafile,usrpath,tallywidth=1,includeEndChar=TRUE){
+    seqset <- Biostrings::readDNAStringSet(fastafile,use.names=FALSE)
     sequence <- unlist(seqset)
     f.col <- getFcolumn(unlist(sequence))
-    l.col <- getLcolumn(sequence)
+    res.list <- getLcolumn(sequence)
+    l.col <- res.list[["bwt"]]
+    l.col <- unlist(l.col)
+    SA <- res.list[["suffix_array"]]
     tally.table <- getTallyTable(l.col,tallywidth)
     
     f.col.filepath <- paste(usrpath,"F_col.txt",collapse="",sep="")
     l.col.filepath <- paste(usrpath,"L_col.txt",collapse="",sep="")
+    SA.filepath <- paste(usrpath,"suffix_array.txt",collapse="",sep="")
     tally.table.filepath <- paste(usrpath,"tally_table.txt",collapse="",sep="")
     
-    FM <- list(f.col,l.col,tally.table)
-    names(FM) <- c("F_column","L_column","tally_table")
+    FM <- list(f.col,l.col,SA,tally.table)
+    names(FM) <- c("F_column","L_column","suffix_array","tally_table")
     
     write.table(t(as.matrix(f.col)), file = f.col.filepath, sep = "\t", 
                 row.names = FALSE)
     write(as.character(l.col),file=l.col.filepath)
+    write(paste(SA, collapse = " "),file=SA.filepath)
     write.table(tally.table, file = tally.table.filepath, 
                 sep = "\t", row.names = FALSE,col.names = TRUE)
-    if(includeSA==TRUE){
-      SA.filepath <- paste(usrpath,"SA.txt",collapse="",sep="")
-      SA <- getSA(sequence)
-      write(SA,file=SA.filepath)
-    }
     
     return(FM)
 }
