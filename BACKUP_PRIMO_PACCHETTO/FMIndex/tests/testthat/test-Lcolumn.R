d4b90cc9ca34aa2c65a23fbf63f3cca768b5b0c3
Sara Resta <sara.resta00@gmail.com>
2023-08-26 15:29:10 +0200

    Update test-Lcolumn.R

--- BACKUP_PRIMO_PACCHETTO/FMIndex/tests/testthat/test-Lcolumn.R
+++ BACKUP_PRIMO_PACCHETTO/FMIndex/tests/testthat/test-Lcolumn.R
@@ -1,5 +1,5 @@
 test_that("Computing the L column (BWT) of a DNA string",{
     sequence <- Biostrings::DNAString("ACGTTACG")
     solution <- Biostrings::DNAString("GT.AACCTG")
-    expect_equal(getLcolumn(sequence),solution)
+    expect_equal(getLcolumn(sequence)[["bwt"]],solution)
 })
