ef2da48a423f960ac993afac13c9d14ce34b7a18
Sara Resta <sara.resta00@gmail.com>
2023-09-07 16:10:10 +0200

    Update steps.sh, run building etc

--- Scripts/steps.sh
+++ Scripts/steps.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+
 echo "writing documentation of the R package"
+
 Rscript "Scripts/write_documentation.R"
 
+
+
 echo "building the vignette of the R package"
+
 Rscript "Scripts/produce_vignette.R"
+
+
 
 echo "build, check and install the package"
 
 
+
+
+
 R CMD build FMIndex
+
 R CMD check FMIndex_0.99.0.tar.gz
+
 R CMD INSTALL FMIndex_0.99.0.tar.gz
 
+
+
 Rscript "Scripts/bioccheck.R"
+
